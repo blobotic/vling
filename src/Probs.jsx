09564faf22b8e0bb70d6ce211a2de0ec409ba542
blobotic <blobotic@tutanota.com>
2023-03-02 22:56:30 -0500

    added a note on contributing in /probs

--- src/Probs.jsx
+++ src/Probs.jsx
@@ -24,9 +24,15 @@ function Probs() {
 			))}
 
 			<h1>Notes</h1>
-			<p><b>NACLO:</b> only 2022 J-R have been populated, without solutions</p>
-			<p><b>IOL:</b> slowly updating w/ solutions</p>
-			<p>Submit a pull request if you'd like to contribute!</p>
+			<p>If anyone has non-official contest problems (NACLO/IOL) written and would like me to add them here, please create a pull request or DM me!</p>
+
+			<p>In the meantime, I'll slowly be adding existing NACLO and IOL problems with solutions—solutions will likely come out at a <i>much</i> slower rate than problem statements.</p>
+
+			<h3>Contributing</h3>
+			<p><b>Adding questions:</b> All pull requests to add questions are welcome! The only guideline is to ensure that everything in the <i>original question statement/PDF</i> exists in the <code>.md</code> file you create; as long as it reasonably mimics the original and is readable, we will accept it.</p>
+			<p><b>Adding solutions:</b> If you are adding a solution to a problem <i>without</i> a solution, create a file called <code>[problem]-sol.md</code> and write up your solution! Make sure to use the sol directive and ensure that everything in the <i>original answer key</i> is contained in your solution somewhere. If the problem already has a solution but you'd like to add a <i>different</i> solution, add a heading labeling each solution with the writer. If you'd like to update/expand on an existing solution, ensure that you are not deleting significant parts of it and submit a pull request!</p>
+			<p><b>Typos/issues/bugs/problems:</b> Please submit a pull request or DM me!</p>
+
 
 		</div>
 		)
