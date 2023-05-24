b3c73405b20af6fdbf8937604a40ea2f62183b3e
blobotic <blobotic@tutanota.com>
2023-05-24 11:34:23 -0400

    make images in toolbar unselectable

---
+++ src/Table.jsx
@@ -0,0 +1,11 @@
+function Table() {
+	// to take markdown tables and make it good =D
+
+	// table columns split by && 
+	// table rows split by \\ 
+	// table title separated by ---
+
+	return ()
+}
+
+export default Table;
